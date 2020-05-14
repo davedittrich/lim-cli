861d6d964e3f155babee8aacf309a3e908923e89
Dave Dittrich <dave.dittrich@gmail.com>
2020-05-13 18:10:09 -0700

    Add interactive selection using bullet

--- lim/packet_cafe/api/status.py
+++ lim/packet_cafe/api/status.py
@@ -5,6 +5,7 @@ import logging
 import textwrap
 
 from cliff.lister import Lister
+from lim.packet_cafe import chose_wisely
 from lim.packet_cafe import get_ids
 from lim.packet_cafe import get_status
 
@@ -29,10 +30,22 @@ class Status(Lister):
     def take_action(self, parsed_args):
         logger.debug('[+] showing status for request')
         ids = get_ids()
-        sess_id = parsed_args.sess_id
-        req_id = parsed_args.req_id
+        if parsed_args.sess_id is not None:
+            sess_id = parsed_args.sess_id
+        else:
+            sess_id = chose_wisely(from_list=ids,
+                                   what="a session",
+                                   cancel_throws_exception=True)
         if sess_id not in ids:
             raise RuntimeError(f'Session ID { sess_id } not found')
+        if parsed_args.req_id is not None:
+            req_id = parsed_args.req_id
+        else:
+            req_id = chose_wisely(
+                from_list=get_request_ids(sess_id=sess_id),
+                what="a request",
+                cancel_throws_exception=True
+            )
         columns = ['Tool', 'State', 'Timestamp']
         data = list()
         status = get_status(sess_id=sess_id, req_id=req_id)
